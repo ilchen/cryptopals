9707118c15bce9699391dda8686bd145d922cdc0
Andrei ILchenko <andrei.ilchenko@gmail.com>
2019-08-18 15:59:48 +0200

    extended documentation

--- src/main/java/com/cryptopals/set_8/DiffieHellmanHelperExt.java
+++ src/main/java/com/cryptopals/set_8/DiffieHellmanHelperExt.java
@@ -13,20 +13,49 @@ import java.util.stream.IntStream;
 import static java.math.BigInteger.ONE;
 import static java.math.BigInteger.ZERO;
 
-public class DiffieHellmanHelperExt extends DiffieHellmanHelper {
-    private static final int   NUM_BITS_Q = 42,  CERTAINTY = 100;
+/**
+ * Represents a group Z<sub>p</sub><sup>*</sup> that is suitable for the Diffie Hellman protocol.
+ * This class allows to construct cryptographically secure instances of Z<sub>p</sub><sup>*</sup> based on the advice
+ * in Section 11.6 of "Cryptography Engineering, 2<sup>nd</sup> edition" by Niels Ferguson, Bruce Schneier,
+ * and Tadayoshi Kohno.
+ */
+final public class DiffieHellmanHelperExt extends DiffieHellmanHelper {
+    private static final int   NUM_BITS_Q = 48,  CERTAINTY = 100;
     private final BigInteger   q;
 
     public static DiffieHellmanHelperExt  newInstance() {
+        return  newInstance(NUM_BITS, NUM_BITS_Q);
+    }
+
+    /**
+     * Constructs a new instance of this class whose Z<sub>p</sub><sup>*</sup> group is defined as follows:
+     * <ul>
+     *     <li>{@code p} is a {@code nBitsP}-bits prime meeting the following  requirement: {@code p = Nq + 1},
+     *     where {@code q} is a {@code nBitsQ}-bits prime.</li>
+     *     <li>The generator {@code g} is a random member of Z<sub>p</sub><sup>*</sup> that has an order of {@code q}</li>
+     * </ul>
+     * @param nBitsP  number of bits in prime {@code p}
+     * @param nBitsQ  number of bits in prome {@code q}
+     */
+    public static DiffieHellmanHelperExt  newInstance(int nBitsP, int nBitsQ) {
+        if (nBitsQ >= nBitsP) {
+            throw  new IllegalArgumentException("Number of bits in p must be greater than those in q");
+        }
+        if (nBitsQ < 20) {
+            throw  new IllegalArgumentException("q must be at least 20 bits");
+        }
+        if (nBitsQ < 128) {
+            throw  new IllegalArgumentException("p must be at least 128 bits");
+        }
         final Random secRandGen = new SecureRandom();
-        BigInteger  q = BigInteger.probablePrime(NUM_BITS_Q, secRandGen),  N,  p,  g;
+        BigInteger  q = BigInteger.probablePrime(nBitsQ, secRandGen),  N,  p,  g;
         do {
-            N = new BigInteger(NUM_BITS - NUM_BITS_Q, secRandGen);
+            N = new BigInteger(NUM_BITS - nBitsQ, secRandGen);
             p = N.multiply(q).add(ONE);
-        } while (p.bitLength() != NUM_BITS  ||  !p.isProbablePrime(CERTAINTY));
+        } while (p.bitLength() != nBitsP  ||  !p.isProbablePrime(CERTAINTY));
 
         do {  // We need to exclude the trivial subgroups
-            g = new BigInteger(NUM_BITS, secRandGen).mod(p).modPow(N, p);
+            g = new BigInteger(nBitsP, secRandGen).mod(p).modPow(N, p);
         } while (g.equals(ONE) || !g.modPow(q, p).equals(ONE));
         return  new DiffieHellmanHelperExt(p, g, q);
     }
